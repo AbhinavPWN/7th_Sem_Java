cdb9764e263c8e83803e35c0505774a4aa50ed4d
AbhinavPWN <104517482+AbhinavPWN@users.noreply.github.com>
2022-11-30 18:52:15 +0545

    JavaFirstCommit

---
+++ OneDimensionalArray.java
@@ -0,0 +1,14 @@
+public class OneDimensionalArray {
+    public static void main(String[] args) {
+        int[] a = new int[3];
+        a[0] = 45;
+        a[1] = 55;
+        a[2] = 65;
+
+        // Printing a array
+        System.out.println("One dimensional array are :");
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a[2]);
+    }
+}
